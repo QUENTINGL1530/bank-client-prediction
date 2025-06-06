9ca8ff549b6d32f0cf32992a0502c6cd06ecdcb1
OMAR1530 <quentin.guillaume17@ynov.com>
2025-06-06 18:23:08 +0200

    cleaning the data set by removing missing data, duplicates and incomplete lines

--- data.py
+++ data.py
@@ -3,6 +3,13 @@ import numpy as np
 
 df = pd.read_excel("Banking_Call_Data.xlsx")
 
+df.replace("unknown", np.nan, inplace=True)
+
+df.dropna(inplace=True)
+
+df.drop_duplicates(inplace=True)
+
+
 print(df.shape)
 print(df.columns)
 print(df.head())
